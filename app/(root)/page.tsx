8c88528e242b411686f4426b21f369a2080d1013
g8844712@gmail.com <g8844712@gmail.com>
2024-09-09 08:33:23 +0100

    implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,43 @@
+import HeaderBox from "@/components/header";
+import RightSideBar from "@/components/RightSideBar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = {
+    firstName: "Genesis",
+    lastName: "Kuba",
+    email: "genesiskuba@gmail.com",
+  };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header ">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || " Genesis"}
+            subtext="Access and manage your account and transactions effeciently."
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+
+
+
+      <RightSideBar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.50 }, { currentBalance: 123.50 }]}
+      />
+    </section>
+  );
 };
 
 export default Home;
---
+++ app/(root)/transaction-history/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TransactionsHistory = () => {
+  return (
+    <div>TransactionHistory</div>
+  )
+}
+
+export default TransactionsHistory
